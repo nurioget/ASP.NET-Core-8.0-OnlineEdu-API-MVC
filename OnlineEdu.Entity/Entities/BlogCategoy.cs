c038676ea46f4805fb3caf31465c6fc50fab419e
nurioget <nurioget9@gmail.com>
2024-10-26 11:57:13 +0300

    Add project files.

---
+++ OnlineEdu.Entity/Entities/BlogCategoy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Entity.Entities
+{
+    public class BlogCategoy
+    {
+        public int BlogCategoryId { get; set; }
+        public string Name { get; set; }
+
+
+        public List<Blog> Blogs { get; set; }
+    }
+}
