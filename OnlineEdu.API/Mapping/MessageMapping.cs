5cd17cba3bbcac1059229fce9d5b1450e7f9b4d0
nurioget <nurioget9@gmail.com>
2024-10-27 11:55:21 +0300

    I made Messages ,SocialMedias ,Subscribers ,Testimonials Controllers

---
+++ OnlineEdu.API/Mapping/MessageMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OnlineEdu.DTO.DTOs.MessageDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Mapping
+{
+    public class MessageMapping : Profile
+    {
+        public MessageMapping()
+        {
+            CreateMap<CreateMessageDto,Message>().ReverseMap();
+            CreateMap<ResultMessageDto,Message>().ReverseMap();
+            CreateMap<UpdateMessageDto,Message>().ReverseMap();
+        }
+    }
+}
