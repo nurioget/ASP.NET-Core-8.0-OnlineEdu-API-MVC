d29f256d18bf0b5e123c039818a43dbe141a0c73
nurioget <nurioget9@gmail.com>
2024-10-28 12:01:15 +0300

    About Admin/Consumption transactions

--- OnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -15,5 +15,36 @@ namespace OnlineEdu.WebUI.Areas.Admin.Controllers
             var values = await _client.GetFromJsonAsync<List<ResultAboutDto>>("abouts");
             return View(values);
         }
+
+        public async Task<IActionResult> DeleteAbout(int id)
+        {
+            await _client.DeleteAsync($"abouts/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+        {
+            await _client.PostAsJsonAsync("abouts", createAboutDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var values = await _client.GetFromJsonAsync<UpdateAboutDto>($"abouts/{id}");
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            await _client.PutAsJsonAsync("abouts", updateAboutDto);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
