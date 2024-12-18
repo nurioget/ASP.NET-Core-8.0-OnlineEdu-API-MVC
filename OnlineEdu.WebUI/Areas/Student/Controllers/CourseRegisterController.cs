338ce6f16a5c9192f337e12c0fc2d55aefb6cc44
nurioget <nurioget9@gmail.com>
2024-12-18 21:05:41 +0300

    Course Register Api Consume in Student Area

--- OnlineEdu.WebUI/Areas/Student/Controllers/CourseRegisterController.cs
+++ OnlineEdu.WebUI/Areas/Student/Controllers/CourseRegisterController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Entity.Entities;
+using OnlineEdu.WebUI.DTOs.CourseRegisterDtos;
+using OnlineEdu.WebUI.Helpers;
 
 namespace OnlineEdu.WebUI.Areas.Student.Controllers
 {
-    public class CourseRegisterController : Controller
+    [Authorize(Roles = "Student")]
+    [Area("Student")]
+    public class CourseRegisterController(UserManager<AppUser> _userManager) : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var values = await _client.GetFromJsonAsync<List<ResultCourseRegisterDto>>("courseRegisters/GetMyCourses/" + user.Id);
+            return View(values);
         }
     }
 }
